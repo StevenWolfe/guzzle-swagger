938fef9f47b3140807d0befcf656eb3ddb01b7ad
Steve Wolfe <wolfe@musicstorelive.com>
2015-02-24 17:52:34 -0500

    Stubbed out step implementations

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -44,4 +44,68 @@ class FeatureContext extends BehatContext
     {
         PHPUnit_Framework_Assert::assertTrue(true);
     }
+
+    /**
+     * @Given /^a SwaggerClient configuration$/
+     */
+    public function aSwaggerClientConfiguration()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the configuration has a valid base_url$/
+     */
+    public function theConfigurationHasAValidBase_url()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I call SwaggerClient\->Factory$/
+     */
+    public function iCallSwaggerClientFactory()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^I should receive a SwaggerClient$/
+     */
+    public function iShouldReceiveASwaggerClient()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the configuration does not have a base_url$/
+     */
+    public function theConfigurationDoesNotHaveABase_url()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^I should receive an error$/
+     */
+    public function iShouldReceiveAnError()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the error should state that a base_url is required$/
+     */
+    public function theErrorShouldStateThatABase_urlIsRequired()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the configuration has an invalid base_url$/
+     */
+    public function theConfigurationHasAnInvalidBase_url()
+    {
+        throw new PendingException();
+    }
 }
